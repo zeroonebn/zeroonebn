6d298681d3e65f6b0a32aa31b32b39b5b3a8883e
Damien Ferrand <df@bigbluebox.ca>
2021-03-29 11:36:30 -0400

    Version 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "xyz.zeroonebn"
-    version = "0.0.1-SNAPSHOT"
+    version = "0.0.1"
 
     extra["springBootVersion"] = "2.4.4"
     extra["jt400Version"] = "10.5"
